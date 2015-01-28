40493a7e4bdb65ae3d018bba0946d599f8dcb8f8
Caleb Adams <CalebAshmoreAdams@gmail.com>
2015-01-27 20:10:09 -0500

    form updates
    the form sends

--- js/contact_me.js
+++ js/contact_me.js
@@ -77,7 +77,7 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    //message: message
                 },
                 cache: false,
                 success: function() {
