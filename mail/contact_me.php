eda868535d2c6e36f0d63750fd97cdd8ba16f3bb
Mason Davenport <masondaven@gmail.com>
2015-01-29 02:52:19 -0500

    cleaning up forms

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,7 +12,7 @@ return false;
 // Create the email and send the message
 $to = 'ugahacks@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form: $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nExtra Information:\n$extrainfo";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nExtra Information: $extrainfo";
 $headers = "From: registration@ugahacks.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email";
 mail($to,$email_subject,$email_body,$headers);
