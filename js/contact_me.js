0a98d93d1ca1255e4eaab11c9b49ecbe834ca5fc
Mason Davenport <masondaven@gmail.com>
2015-01-29 02:39:27 -0500

    attempt to fix parse

--- js/contact_me.js
+++ js/contact_me.js
@@ -3,7 +3,6 @@
 $(".dropdown-menu li a").click(function(){
   var selText = $(this).text();
   $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
-  $("btn-group").css({"color":"black"});
 });
 
 $("#btnSearch").click(function(){
