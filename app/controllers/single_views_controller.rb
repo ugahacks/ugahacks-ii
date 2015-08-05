6999485594b31547a7b4e9fb7b7216e5af469963
Jaicob <jaicob@icloud.com>
2015-08-05 13:07:37 -0500

    Update single_views_controller.rb

--- app/controllers/single_views_controller.rb
+++ app/controllers/single_views_controller.rb
@@ -5,21 +5,4 @@ class SingleViewsController < ApplicationController
   def home
   end
 
-  # def subscribe
-
-  #   @list_id = "3b813d061b"
-  #   gb = Gibbon::Request.new
-  #   begin
-  #     gb.lists(@list_id).members.create(body: {
-  #                                        email_address: params['email'],
-  #                                        status: "subscribed",
-  #     })
-  #     redirect_to root_path, :flash => { success: "Thanks for subscribing we'll keep you posted!" }
-  #   rescue Gibbon::MailChimpError => e
-  #     return redirect_to root_path, :flash => { error: e.message }
-  #   end
-
-  # end
-
-
 end
